a63c026a02ed29a54527f0ab1c4490677fc78e89
Roberto Toribio <roberttoribio04@gmail.com>
2023-04-03 20:32:50 -0700

    Button Landmark issue fixed

--- pygame_gui/gui1.py
+++ pygame_gui/gui1.py
@@ -48,7 +48,7 @@ GREY = (128,128,128)
 GREEN = (127,255,0)
 BLACK = (0, 0, 0)
 RED = (255,0,0)
-
+CYAN = (0, 255, 255)
 
 
 
@@ -65,49 +65,49 @@ button_clicked_color=GREEN
 button1_x = 200
 button1_y =200
 button1_text = "left shoulder"
-button1_clicked = False
+button1_on = False
 
 button2_x = 400
 button2_y = 200
 button2_text = "right shoulder"
-button2_clicked = False
+button2_on = False
 
 button3_x = 200
 button3_y = 300
 button3_text = "left elbow"
-button3_clicked = False
+button3_on = False
 
 button4_x = 400
 button4_y = 300
 button4_text = "right elbow"
-button4_clicked = False
+button4_on = False
 
 button5_x = 200
 button5_y = 400
 button5_text = "left wrist"
-button5_clicked = False
+button5_on = False
 
 button6_x = 400
 button6_y = 400
 button6_text = "right wrist"
-button6_clicked = False
+button6_on = False
 
 button7_x = 200
 button7_y = 500
 button7_text = "left pinky"
-button7_clicked = False
+button7_on = False
 
 button8_x = 400
 button8_y = 500
 button8_text = "right pinky"
-button8_clicked = False
+button8_on = False
 
 
 
 on_engine_button_x  = 600
 on_engine_button_y = 200
 on_engine_button_text = "Start webcam"
-on_engine_button_clicked = False
+engine_button_on = False
 on_button_clicked_color = RED;
 
 user_landmark_choices = []
@@ -117,47 +117,78 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_pos = pygame.mouse.get_pos()
             if is_button_clicked(button1_x,button1_y,button_width,button_height):
-                button1_clicked = not button1_clicked
-                user_landmark_choices.append(l_shoulder_idx);
+                if button1_on: #then turn off (ie remove the limb)
+                    user_landmark_choices.remove(l_shoulder_idx)
+                    button1_on = not button1_on
+                else:
+                    button1_on = not button1_on
+                    user_landmark_choices.append(l_shoulder_idx);
+    
             if is_button_clicked(button2_x,button2_y,button_width,button_height):
-                button2_clicked = not button2_clicked
-                user_landmark_choices.append(r_shoulder_idx)
+                if button2_on:
+                    user_landmark_choices.remove(r_shoulder_idx)
+                    button2_on = not button2_on
+                else:
+                    button2_on = not button2_on
+                    user_landmark_choices.append(r_shoulder_idx)
             if is_button_clicked(button3_x,button3_y,button_width,button_height):
-                button3_clicked = not button3_clicked
-                user_landmark_choices.append(l_elbow_idx)
+                if button3_on:
+                    button3_on = not button3_on
+                    user_landmark_choices.remove(l_elbow_idx)
+                else:
+                    button3_on = not button3_on
+                    user_landmark_choices.append(l_elbow_idx)
             if is_button_clicked(button4_x,button4_y,button_width,button_height):
-                button4_clicked = not button4_clicked
-                user_landmark_choices.append(r_elbow_idx)
+                 if button4_on:
+                    button4_on= not button4_on
+                    user_landmark_choices.remove(r_elbow_idx)
+                 else:
+                    button4_on = not button4_on
+                    user_landmark_choices.append(r_elbow_idx)
             if is_button_clicked(button5_x,button5_y,button_width,button_height):
-                button5_clicked = not button5_clicked
-                user_landmark_choices.append(l_wrist_idx)
+                if button5_on:
+                    button5_on = not button5_on
+                    user_landmark_choices.remove(l_wrist_idx)
+                else:
+                    button5_on = not button5_on
+                    user_landmark_choices.append(l_wrist_idx)
             if is_button_clicked(button6_x,button6_y,button_width,button_height):
-                button6_clicked = not button6_clicked
-                user_landmark_choices.append(r_wrist_idx)
+                if button6_on:
+                    button6_on = not button6_on
+                    user_landmark_choices.remove(r_wrist_idx)
+                else:
+                    button6_on = not button6_on
+                    user_landmark_choices.append(r_wrist_idx)
             if is_button_clicked(button7_x,button7_y,button_width,button_height):
-                button7_clicked = not button7_clicked
-                user_landmark_choices.append(l_pinky_idx)
+                if button7_on:
+                    button7_on = not button7_on
+                    user_landmark_choices.remove(l_pinky_idx)
+                else:
+                    button7_on = not button7_on
+                    user_landmark_choices.append(l_pinky_idx)
             if is_button_clicked(button8_x,button8_y,button_width,button_height):
-                button8_clicked = not button8_clicked
-                user_landmark_choices.append(r_pinky_idx)
+                if button8_on:
+                    button8_on = not button8_on
+                    user_landmark_choices.remove(r_pinky_idx)
+                else:
+                    button8_on = not button8_on
+                    user_landmark_choices.append(r_pinky_idx)
            
             #the last check is to see if we can create the new screen for the webcam stuff
             if is_button_clicked(on_engine_button_x, on_engine_button_y,button_width,button_height):
-                on_engine_button_clicked = not on_engine_button_clicked
+                engine_button_on = not engine_button_on
                 myEngine = MediaPipeEngine(webcam_id = 0, show=True, custom_objects=None, user_landmark_list=user_landmark_choices);
                 myEngine.run()
-                button_on_list = [button1_clicked,button2_clicked, button3_clicked,
-                                  button4_clicked,button5_clicked,button6_clicked, button7_clicked,
-                                  button8_clicked, on_engine_button_clicked ]
-                button1_clicked = reset_button(button1_clicked)
-                button2_clicked = reset_button(button2_clicked)
-                button3_clicked = reset_button(button3_clicked)
-                button4_clicked = reset_button(button4_clicked)
-                button5_clicked = reset_button(button5_clicked)
-                button6_clicked = reset_button(button6_clicked)
-                button7_clicked = reset_button(button7_clicked)
-                button8_clicked = reset_button(button8_clicked)
-                on_engine_button_clicked = reset_button (on_engine_button_clicked)
+            
+                button1_on = reset_button(button1_on)
+                button2_on = reset_button(button2_on)
+                button3_on = reset_button(button3_on)
+                button4_on = reset_button(button4_on)
+                button5_on = reset_button(button5_on)
+                button6_on = reset_button(button6_on)
+                button7_on = reset_button(button7_on)
+                button8_on = reset_button(button8_on)
+                engine_button_on = reset_button (engine_button_on)
                 user_landmark_choices = []
                 print("ok the webcam is off now ")
 
@@ -165,14 +196,14 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    screen.fill(WHITE)
-    draw_button(screen,button_color,button_clicked_color,button1_x,button1_y, button_width, button_height, button1_text, button1_clicked)
-    draw_button(screen,button_color,button_clicked_color,button2_x,button2_y, button_width, button_height, button2_text, button2_clicked)
-    draw_button(screen,button_color,button_clicked_color,button3_x,button3_y, button_width, button_height, button3_text, button3_clicked)
-    draw_button(screen,button_color,button_clicked_color,button4_x,button4_y, button_width, button_height, button4_text, button4_clicked)
-    draw_button(screen,button_color,button_clicked_color,button5_x,button5_y, button_width, button_height, button5_text, button5_clicked)
-    draw_button(screen,button_color,button_clicked_color,button6_x,button6_y, button_width, button_height, button6_text, button6_clicked)
-    draw_button(screen,button_color,button_clicked_color,button7_x,button7_y, button_width, button_height, button7_text, button7_clicked)
-    draw_button(screen,button_color,button_clicked_color,button8_x,button8_y, button_width, button_height, button8_text, button8_clicked)
-    draw_button(screen, button_color, button_clicked_color, on_engine_button_x, on_engine_button_y, button_width,button_height, on_engine_button_text, on_engine_button_clicked)
+    screen.fill(CYAN)
+    draw_button(screen,button_color,button_clicked_color,button1_x,button1_y, button_width, button_height, button1_text, button1_on)
+    draw_button(screen,button_color,button_clicked_color,button2_x,button2_y, button_width, button_height, button2_text, button2_on)
+    draw_button(screen,button_color,button_clicked_color,button3_x,button3_y, button_width, button_height, button3_text, button3_on)
+    draw_button(screen,button_color,button_clicked_color,button4_x,button4_y, button_width, button_height, button4_text, button4_on)
+    draw_button(screen,button_color,button_clicked_color,button5_x,button5_y, button_width, button_height, button5_text, button5_on)
+    draw_button(screen,button_color,button_clicked_color,button6_x,button6_y, button_width, button_height, button6_text, button6_on)
+    draw_button(screen,button_color,button_clicked_color,button7_x,button7_y, button_width, button_height, button7_text, button7_on)
+    draw_button(screen,button_color,button_clicked_color,button8_x,button8_y, button_width, button_height, button8_text, button8_on)
+    draw_button(screen, button_color, button_clicked_color, on_engine_button_x, on_engine_button_y, button_width,button_height, on_engine_button_text, engine_button_on)
     pygame.display.update()
